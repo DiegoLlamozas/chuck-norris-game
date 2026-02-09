2e3067f2119b8c900124340c9f043078010179e9
DiegoLlamozas <diegollamozas911@gmail.com>
2026-02-08 20:37:17 -0500

    feat(ui): add footer component with copyright and attribution.

---
+++ src/shared/presentation/components/footer-content.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+
+</script>
+
+<template>
+  <div class="grid mt-4 p-2 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2026. Edson Diego Llamozas Diaz</p>
+    </div>
+    <div class="col-12 ml-3 mt-1 align-items-center justify-content-center">
+      <p>
+        Made with <i class="pi pi-heart"/>
+        using <a href="https://primevue.org/" target="_blank">PrimeVue</a>
+        by Edson Diego Llamozas Diaz Development Team
+      </p>
+
+      <a href="https://www.flaticon.com/free-icons/fight" title="fight icons">Fight icons created by Freepik - Flaticon</a>
+    </div>
+  </div>
+
+</template>
+
+<style scoped>
+
+p{
+  color: var(--color-lavender-300);
+}
+
+</style>
