3d8caf3fd83935575987974dcdd9e674f49d6be7
DiegoLlamozas <diegollamozas911@gmail.com>
2026-02-08 20:37:32 -0500

    feat(ui): add 404 page component for unavailable routes.

---
+++ src/shared/presentation/views/page-not-found.vue
@@ -0,0 +1,18 @@
+<script setup>
+import {useRouter} from "vue-router";
+
+const router = useRouter();
+const unavailableRoute = router.currentRoute.value.fullPath;
+</script>
+
+<template>
+  <div class="align-content-start justify-content-start m-4">
+    <h1>Page Not Found</h1>
+    <p>{{ `The path ${unavailableRoute} you are looking for does not exist.` }}</p>
+    <router-link to="/jokes">Go Home</router-link>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
